ef40be9c800e9499172de7b701f5564d791ee727
Gautier MICHELIN <gm@ideesculture.com>
2023-08-19 12:24:26 +0200

    await on root is not authorized with chrome & safari targets

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ import Dexie from 'dexie';
 export const db = new Dexie('collectiveaccessOffline');
 
 // Création du premier store en suivant la doc de Dexie
-await db.open();
+db.open();
 console.log("Numéro de version ", db.verno);
 db.close();
 db.version(1).stores({ settings: 'id, type, table, data' });
@@ -14,19 +14,19 @@ db.version(1).stores({ settings: 'id, type, table, data' });
 // du coup, on clos, on ouvre, on clos, on ouvre, etc. en upgradant le numéro de version
 db.close();
 db.version(2).stores({db_objects: 'id,idno,data' });
-await db.open();
+db.open();
 db.close();
 db.version(3).stores({ db_entities: 'id,idno,data' });
-await db.open();
+db.open();
 db.close();
 db.version(4).stores({ db_collections: 'id,idno,data' });
-await db.open();
+db.open();
 db.close();
 db.version(5).stores({ db_places: 'id,idno,data' });
-await db.open();
+db.open();
 db.close();
 db.version(6).stores({ db_occurrences: 'id,idno,data' });
-await db.open();
+db.open();
 db.close();
 db.version(7).stores({ db_storage_locations: 'id,idno,data' });
-await db.open();
+db.open();
